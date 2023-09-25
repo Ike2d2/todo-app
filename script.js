cc199597b1a87306c881425e9c0a91ed0b88f157
Ike2d2 <ike2d2@thefuturesmind.com>
2023-09-25 11:30:22 -0600

    Add List Item Popup

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let list = document.getElementById("todoList");
 let mainHead = document.getElementById("mainHead");
 
 let selected = 0;
-function setSelected(x){
+function setSelected(x) {
     selected = x;
     renderList();
 }
 
 function renderList() {
     list.innerHTML = null;
-    new Array(25).fill().forEach((e, i) => {
+    new Array(9).fill().forEach((e, i) => {
+        let frag = document.createDocumentFragment();
+
         let item = document.createElement("div");
-        item.className = `flex flex-col h-20 w-full flex-shrink-0 p-1 border-b-2${i === selected ? ' selected' : ''}`;
+        item.className = `flex flex-col h-20 w-full flex-shrink-0 px-1 border-b-2${i === selected ? ' selected' : ''}`;
         item.setAttribute('key', i);
 
         item.addEventListener('click', () => {
@@ -23,13 +25,30 @@ function renderList() {
         span1.innerText = "List " + (i + 1);
 
         let span2 = document.createElement("span");
-        span2.className = "text-sm";
-        span2.innerText = "List Contents Preview List Contents Preview List Contents Preview List Contents Preview"
+        span2.className = "text-sm line-clamp-2 overflow-hidden";
+        span2.innerText = "List Contents Preview List Contents Preview List Contents Preview List Contents Preview List Contents Preview List Contents Preview List Contents Preview List Contents Preview"
 
         item.append(span1, span2);
-        list.append(item);
+        frag.append(item);
+        list.append(frag);
     })
     mainHead.innerText = "List " + (selected + 1)
 }
 
 renderList()
+
+let addListPopup = document.getElementById("addListPopup");
+let addListInput = document.getElementById("addListInput");
+
+addList.addEventListener("click", (e) => {
+    addListInput.value= "";
+    addListPopup.classList.toggle('hidden');
+    addListInput.focus();
+
+    const id = setInterval(() => {
+        if(document.activeElement !== addListInput){
+            addListPopup.classList.toggle('hidden');
+            clearInterval(id);
+        }
+    }, 100)
+})
