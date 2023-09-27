6a98487253e3c07a4cd5e9ab58ccf3cc31045f32
Ike2d2 <ike2d2@thefuturesmind.com>
2023-09-27 10:11:01 -0600

    Localized Icons, Ability to add Tasks, and Row Icons

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
+"use strict"
+
 const list = document.getElementById("todoList");
 const mainHead = document.getElementById("mainHead");
 
 let selected = 0;
 function setSelected(x) {
-  selected = x;
-  renderList();
-  renderMain();
+    selected = x;
+    renderList();
+    renderMain();
 }
 
 const listItems = [];
 function addListItem(x) {
-  listItems.push({
-    title: x,
-    items: [
-      {
-        contents: "Do the Dishes",
-        done: false,
-      },
-      {
-        contents: "Do Homework",
-        done: true,
-      },
-    ],
-  });
-  renderList();
-  renderMain();
+    listItems.push({
+        title: x,
+        items: [],
+    });
+    renderList();
+    renderMain();
 }
 
 function renderList() {
-  list.innerHTML = null;
-  listItems.forEach((e, i) => {
-    const item = document.createElement("div");
-    item.className = `flex flex-col h-20 w-full flex-shrink-0 px-1 border-b-2${
-      i === selected ? " selected" : ""
-    }`;
-    item.setAttribute("key", i);
+    list.innerHTML = null;
+    listItems.forEach((e, i) => {
+        const item = document.createElement("div");
+        item.className = `flex flex-col h-20 w-full flex-shrink-0 px-1 border-b-2${i === selected ? " selected" : ""
+            }`;
 
-    item.addEventListener("click", () => {
-      setSelected(i);
+        item.addEventListener("click", () => {
+            setSelected(i);
+        });
+
+        const span1 = document.createElement("span");
+        span1.className = "font-bold text-lg";
+        span1.innerText = e.title;
+
+        const buttons = document.createElement('div');
+        buttons.className = 'buttons flex items-center justify-center text-3xl';
+
+        const edit = document.createElement('i');
+        edit.className = "ri-edit-line text-neutral-300";
+
+        const trash = document.createElement('i');
+        trash.className = "ri-delete-bin-6-line text-neutral-300";
+
+        buttons.append(edit, trash);
+        item.append(span1, buttons);
+        list.append(item);
     });
-
-    const span1 = document.createElement("span");
-    span1.className = "font-bold text-lg";
-    span1.innerText = e.title;
-
-    item.append(span1);
-    list.append(item);
-  });
 }
 
 const addListPopup = document.getElementById("addListPopup");
@@ -54,43 +55,69 @@ const addListInput = document.getElementById("addListInput");
 const addListButton = document.getElementById("addListButton");
 
 addListInput.addEventListener("blur", () => {
-  addListPopup.classList.add("hidden");
+    addListPopup.classList.add("hidden");
 });
 
-addList.addEventListener("pointerdown", (e) => {
-  addListInput.value = "";
-  addListPopup.classList.remove("hidden");
-  setTimeout(() => {
-    addListInput.focus();
-  });
+addListInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        addListInput.value && addListItem(addListInput.value);
+        addListInput.blur();
+    }
 });
 
-addListInput.addEventListener("blur", () => {
-  addListPopup.classList.add("hidden");
+const addList = document.getElementById('addList');
+addList.addEventListener("pointerdown", () => {
+    addListInput.value = "";
+    addListPopup.classList.remove("hidden");
+    setTimeout(() => {
+        addListInput.focus();
+    });
 });
 
 addListButton.addEventListener(
-  "pointerdown",
-  () => addListInput.value && addListItem(addListInput.value)
+    "pointerdown",
+    () => addListInput.value && addListItem(addListInput.value)
 );
-addListInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    addListInput.value && addListItem(addListInput.value);
-    addListInput.blur();
-  }
-});
 
 const mainBody = document.getElementById('mainBody');
+
+function addTaskItem() {
+    listItems[selected].items.push({
+        contents: '',
+        done: false
+    })
+    renderMain();
+}
 
 function renderMain() {
     const toDisplay = listItems[selected];
     mainBody.innerHTML = null;
     mainHead.innerText = toDisplay.title;
     const ul = document.createElement('ul');
-    toDisplay.items.forEach((e) => {
-        const li = document.createElement('li');
-        li.innerText = e.contents;
-        ul.append(li);
+    toDisplay.items.forEach((e, i) => {
+        const div = document.createElement('div');
+        div.innerText = e.contents;
+        div.className = 'flex items-center justify-center h-20 border-b-2';
+
+        const buttons = document.createElement('div');
+        buttons.className = 'buttons flex items-center justify-center text-3xl';
+
+        const edit = document.createElement('i');
+        edit.className = "ri-edit-line text-neutral-300";
+
+        const trash = document.createElement('i');
+        trash.className = "ri-delete-bin-6-line text-neutral-300";
+
+        buttons.append(edit, trash);
+        div.append(buttons)
+        ul.append(div);
     })
+    const addTask = document.createElement('div');
+    addTask.className = 'flex items-center justify-center h-20 flex-shrink-0'
+    addTask.addEventListener('click', addTaskItem);
+    const i = document.createElement('i');
+    i.className = 'ri-add-fill text-neutral-300 text-5xl';
+    addTask.append(i)
     mainBody.append(ul);
+    mainBody.append(addTask);
 }
