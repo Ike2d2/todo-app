1c6b578d424f649c4cee97b51988b120c5920e3b
Isaac Stark <isaac.stark@lifebeacon.com>
2023-10-11 11:48:06 -0600

    Custom Scrollbars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-content: ["*.{html,js}"],
+  darkMode: "class",
+  content: ["*.{html,js}", "modules/*.js"],
   theme: {
     extend: {
       colors: {
-        primary: 'rgb(63,63,70)',
-        secondary: '#eeeeee',
-        'sel-light': '#e0e0e0',
-        'sel-dark': '#313137',
-        lighter: '#fafafa',
-        darker: '#36363c'
+        primary: "rgb(63,63,70)",
+        secondary: "#eeeeee",
+        "sel-light": "#e0e0e0",
+        "sel-dark": "#313137",
+        lighter: "#fafafa",
+        darker: "#36363c",
       },
       boxShadow: {
-        light: '-30px 30px 60px #cacaca, 30px -30px 60px #ffffff',
-        dark: '-30px 30px 60px #36363c, 30px -30px 60px #484851'
-      }
+        light: "-30px 30px 60px #cacaca, 30px -30px 60px #ffffff",
+        dark: "-30px 30px 60px #36363c, 30px -30px 60px #484851",
+      },
     },
   },
   plugins: [],
-}
-
+};
